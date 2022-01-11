1d82addcb9ee22f47d3cf3be5e5f94c50d01d18e
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-11 13:00:16 +0600

    Show value from form

--- frontend-nextjs/pages/index.js
+++ frontend-nextjs/pages/index.js
@@ -1,14 +1,34 @@
-import { usestate } from 'react'
+import { useState } from 'react'
 
 const Home = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
 
+  const onChangeName = (evt) => {
+    setName(evt.target.value)
+  }
+
+  const onChangeEmail = (evt) => {
+    setEmail(evt.target.value)
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    console.log(name, email)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={sendEmail} method='post'>
         <h1>Formula 1</h1>
-        <input type='text' name='name' value={name} />
+        <input type='text' name='name' value={name} onChange={onChangeName} />
+        <input
+          type='text'
+          name='email'
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <button>Submit</button>
       </form>
     </div>
   )
